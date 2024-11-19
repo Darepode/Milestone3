b339238dd1af72c69b4b2733a3d81b299ebe035d
Darepode <phamm245@gmail.com>
2024-11-19 22:29:51 +0700

    Update BRU

--- Non-Forwarding/10_sim/Filelist.f
+++ Non-Forwarding/10_sim/Filelist.f
@@ -1,14 +1,20 @@
-../00_src/adder.sv
-../00_src/alu.sv
-../00_src/brcomp.sv
-../00_src/ctrl_unit.sv
-../00_src/imem.sv
-../00_src/immgen.sv
-../00_src/lsu.sv
-../00_src/regfile.sv
-../00_src/singlecycle.sv
-../00_src/sll.sv
-../00_src/sra.sv
-../00_src/srl.sv
+// ../00_src/adder.sv
+// ../00_src/alu.sv
+// ../00_src/brcomp.sv
+// ../00_src/ctrl_unit.sv
+// ../00_src/imem.sv
+// ../00_src/immgen.sv
+// ../00_src/lsu.sv
+// ../00_src/regfile.sv
+// ../00_src/singlecycle.sv
+// ../00_src/sll.sv
+// ../00_src/sra.sv
+// ../00_src/srl.sv
+// ../00_src/ctrl_fsm.sv
 
-../01_bench/singlecycle_tb.sv
+// ../01_bench/singlecycle_tb.sv
+
+// Test BRU
+../00_src/bru.sv
+../01_bench/bru_tb.sv
+
