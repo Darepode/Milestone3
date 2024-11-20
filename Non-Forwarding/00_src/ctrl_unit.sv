93c6ba41132c18198801b4c158ebf65f2055f2fd
GiaHuyNguyen13 <giahuy130302003@gmail.com>
2024-11-20 13:43:37 +0700

    Update ctrl_unit (instr_valid is incorrect)

--- Non-Forwarding/00_src/ctrl_unit.sv
+++ Non-Forwarding/00_src/ctrl_unit.sv
@@ -1,11 +1,10 @@
 module ctrl_unit (
 	input  wire [31:0] instr,
-	input  wire [0:0]  br_less, br_equal, 
-	output reg  [2:0]  l_length,
-    output reg  [0:0]  br_sel, br_unsigned, rd_wren, mem_wren, mem_rden, op_a_sel, op_b_sel, insn_vld,  l_unsigned,
-	output reg  [1:0]  wb_sel, s_length,
+    output reg  [0:0]  rd_wren, mem_wren, mem_rden, op_a_sel, insn_vld, is_br, wb_sel,
+	output reg  [1:0]  is_uncbr, op_b_sel,
 	output reg  [3:0]  alu_op
 );
+
 wire [6:0] func7;
 wire [2:0] func3;
 parameter  R_type       = 7'b0110011,
@@ -25,11 +24,11 @@ always @(*) begin
 	case (instr[6:0])
 		R_type      : begin
 					  if (!(func7 == 7'b0 || func7 == 7'b0100000)) begin
-						br_sel = 1'b0; br_unsigned = 1'b0; rd_wren = 1'b0; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 1'b0; wb_sel = 2'b00;
-					    l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b0;
+						rd_wren = 1'b0; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b0; wb_sel = 1'b0;
+					    is_br = 1'b0; is_uncbr = 2'b00; insn_vld = 1'b0;
 					  end else begin
-						br_sel = 1'b0; br_unsigned = 1'b0; rd_wren = 1'b1; mem_wren = 1'b0; op_a_sel = 1'b0; op_b_sel = 1'b0; wb_sel = 2'b00;
-					  l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b1;
+						rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b0; wb_sel = 1'b0;
+					    is_br = 1'b0; is_uncbr = 2'b00; insn_vld = 1'b1;
 					  end
 						case (func3)
 							3'b000: alu_op = (!func7[5]) ? 4'b0000 : 4'b0001;
@@ -43,8 +42,8 @@ always @(*) begin
 						endcase
 		end
 		I_type_IMM  : begin
-			          br_sel = 1'b0; br_unsigned = 1'b0; rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 1'b1; wb_sel = 2'b00;
-					  l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b1;
+			          rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b01; wb_sel = 1'b0;
+					  is_br = 1'b0; is_uncbr = 2'b00; insn_vld = 1'b1;
 					  case (func3)
 					    3'b000: alu_op = 4'b0000;
 						3'b010: alu_op = 4'b0010;
@@ -57,72 +56,36 @@ always @(*) begin
 					  endcase
 		end
 		I_type_JALR : begin
-			          br_sel = 1'b1; br_unsigned = 1'b0; rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 1'b0; wb_sel = 2'b10;
-					  alu_op = 4'b0000; l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b1;
+			          rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b10; wb_sel = 1'b0;
+					  is_br = 1'b0; is_uncbr = 2'b11; alu_op = 4'b1010; insn_vld = 1'b1; 
 		end
 		I_type_LD   : begin
-			          br_sel = 1'b0; br_unsigned = 1'b0; rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b1; op_a_sel = 1'b0; op_b_sel = 1'b1; wb_sel = 2'b01;
-					  alu_op = 4'b0000; s_length = 2'b0; l_length = func3; insn_vld = 1'b1;
-					  if (func3 == 3'b100 || func3 == 3'b101) l_unsigned = 1'b1;
-					  else l_unsigned = 1'b0;
+			          rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b1; op_a_sel = 1'b0; op_b_sel = 2'b01; wb_sel = 1'b1;
+					  is_br = 1'b0; is_uncbr = 2'b00; alu_op = 4'b0000; insn_vld = 1'b1;
 		end
         B_type      : begin
-                      /*br_sel = 1'b1;*/  rd_wren = 1'b0; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b1; op_b_sel = 1'b1; wb_sel = 2'b00; alu_op = 4'b0000;
-					  l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b1;
-                      case (func3)
-					    3'b000: begin   // beq
-							br_unsigned = 1'b0;
-							if (br_equal) br_sel = 1'b1;
-							else br_sel = 1'b0;
-						end
-						3'b001: begin  //bne
-							br_unsigned = 1'b0;
-							if (!br_equal) br_sel = 1'b1;
-							else br_sel = 1'b0;
-						end
-						3'b100: begin   //blt
-							br_unsigned = 1'b0;
-							if (br_less) br_sel = 1'b1;
-							else br_sel = 1'b0;
-						end
-						3'b101: begin   //bge
-							br_unsigned = 1'b0;
-							if (!br_less) br_sel = 1'b1;
-							else br_sel = 1'b0;
-						end
-						3'b110: begin   //bltu
-							br_unsigned = 1'b1;
-							if (br_less) br_sel = 1'b1;
-							else br_sel = 1'b0;
-						end
-						3'b111: begin   //bgeu
-							br_unsigned = 1'b1;
-							if (!br_less) br_sel = 1'b1;
-							else br_sel = 1'b0;
-						end
-					  endcase  
+                      rd_wren = 1'b0; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b00; wb_sel = 1'b0;
+					  is_br = 1'b1; is_uncbr = 2'b00; alu_op = 4'b0000; insn_vld = 1'b1;
 		end
 		S_type      : begin
-                      br_sel = 1'b0;  br_unsigned = 1'b0; rd_wren = 1'b0; mem_wren = 1'b1; mem_rden = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 1'b1; wb_sel = 2'b00; alu_op = 4'b0000;
-					  s_length = func3[1:0]; l_unsigned = 1'b0; l_length = 3'b0; insn_vld = 1'b1;
-					  
+                      rd_wren = 1'b0; mem_wren = 1'b1; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b01; wb_sel = 1'b0;
+					  is_br = 1'b0; is_uncbr = 2'b00; alu_op = 4'b0000; insn_vld = 1'b1;
 		end
 		J_type      : begin
-                      br_sel = 1'b1;  br_unsigned = 1'b0; rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b1; op_b_sel = 1'b1; wb_sel = 2'b10; alu_op = 4'b0000;
-					  l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b1;
+                      rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b10; wb_sel = 1'b0;
+					  is_br = 1'b0; is_uncbr = 2'b10; alu_op = 4'b1010; insn_vld = 1'b1;
 		end
 		U_type_LUI  : begin
-                      br_sel = 1'b0;  br_unsigned = 1'b0; rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 1'b1; wb_sel = 2'b00; alu_op = 4'b1010;
-					  l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b1;
+                      rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b01; wb_sel = 1'b0;
+					  is_br = 1'b0; is_uncbr = 2'b00; alu_op = 4'b1010; insn_vld = 1'b1;
 		end
 		U_type_AUIPC: begin
-                      br_sel = 1'b0;  br_unsigned = 1'b0; rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b1; op_b_sel = 1'b1; wb_sel = 2'b00; alu_op = 4'b0000;
-					  l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b1;
+                      rd_wren = 1'b1; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b1; op_b_sel = 2'b01; wb_sel = 1'b0;
+					  is_br = 1'b0; is_uncbr = 2'b00; alu_op = 4'b0000; insn_vld = 1'b1;
 		end
 		default: begin
-			br_sel = 1'b0; br_unsigned = 1'b0; rd_wren = 1'b0; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 1'b0; wb_sel = 2'b00;
-			l_unsigned = 1'b0; l_length = 3'b0; s_length = 2'b0; insn_vld = 1'b0;
-		 	//insn_vld = 1'b1;
+					  rd_wren = 1'b0; mem_wren = 1'b0; mem_rden = 1'b0; op_a_sel = 1'b0; op_b_sel = 2'b00; wb_sel = 1'b0;
+					  is_br = 1'b0; is_uncbr = 2'b00; alu_op = 4'b0000; insn_vld = 1'b0;
 		end
                     
 	endcase
