58619c03553a5661118327f263bb569936ad3e2c
GiaHuyNguyen13 <giahuy130302003@gmail.com>
2024-11-22 09:06:56 +0700

    Update hdu name

--- Non-Forwarding/00_src/blocks_reuse/hdu.sv
+++ Non-Forwarding/00_src/blocks_reuse/hdu.sv
@@ -1,27 +1,27 @@
 module hdu (
-    input logic pcsel_exmem, is_br_exmem, is_uncbr_exmem, 
-                rdwren_idex, rdwren_exmem,      
-    input logic [4:0] rd_idex, rd_exmem, rs1_ifid, rs2_ifid,
+    input logic EXMEM_pcsel, EXMEM_is_br, EXMEM_is_uncbr, 
+                IDEX_rdwren, EXMEM_rdwren,      
+    input logic [4:0] IDEX_rd, EXMEM_rd, IFID_rs1, IFID_rs2,
 
-    output logic clear_ifid, clear_idex,
-                 clear_exmem, wren_ifid, pc_wren  
+    output logic IFID_clear, IDEX_clear,
+                 EXMEM_clear, IFID_wren, pc_wren  
 );
     always @(*) begin
-        if(pcsel_exmem && (is_br_exmem || is_uncbr_exmem)) begin
-            clear_ifid  = 1'b1;
-            clear_idex  = 1'b1;
-            clear_exmem = 1'b1;
-        end else if((rdwren_idex && (rd_idex == rs1_ifid || rd_idex == rs2_ifid)) || 
-                    (rdwren_exmem && (rd_exmem == rs1_ifid || rd_exmem == rs2_ifid))) begin
+        if(EXMEM_pcsel && (EXMEM_is_br || EXMEM_is_uncbr)) begin
+            IFID_clear  = 1'b1;
+            IDEX_clear  = 1'b1;
+            EXMEM_clear = 1'b1;
+        end else if((IDEX_rdwren && (IDEX_rd == IFID_rs1 || IDEX_rd == IFID_rs2)) || 
+                    (EXMEM_rdwren && (EXMEM_rd == IFID_rs1 || EXMEM_rd == IFID_rs2))) begin
                         pc_wren    = 1'b0;
-                        wren_ifid  = 1'b0;
-                        clear_idex = 1'b1;
+                        IFID_wren  = 1'b0;
+                        IDEX_clear = 1'b1;
         end else begin
-            clear_ifid  = 1'b0;
-            clear_idex  = 1'b0;
-            clear_exmem = 1'b0;
+            IFID_clear  = 1'b0;
+            IDEX_clear  = 1'b0;
+            EXMEM_clear = 1'b0;
             pc_wren     = 1'b1;
-            wren_ifid   = 1'b1;           
+            IFID_wren   = 1'b1;           
         end
     end
 
